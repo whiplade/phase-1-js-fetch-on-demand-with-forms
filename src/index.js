4a20bb2f9adba75033f38f4b7746a31bbfaeefe9
whiplade <nmawira13@gmail.com>
2023-06-26 11:42:56 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 const init = () => {
-  
+    const inputForm = document.querySelector('form');
+
+    inputForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        const input = document.querySelector('input#searchByID');
+        console.log(input.value);
+
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then(res => res.json())
+        .then(data => {
+
+        let title = document.querySelector('section#movieDetails h4')
+        let summary = document.querySelector('section#movieDetails p')
+        
+        title.innerHTML = data.title
+        summary.innerHTML = data.summary
+
+        });
+        
+
+    });
 }
 
 document.addEventListener('DOMContentLoaded', init);
